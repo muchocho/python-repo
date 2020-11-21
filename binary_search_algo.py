c430ba002bcc78143c3c179cac6395d0d62fbea6
muchocho <43216302+muchocho@users.noreply.github.com>
2020-11-21 22:31:04 +0800

    Add files via upload

---
+++ binary_search_algo.py
@@ -0,0 +1,18 @@
+def bin_search(sequence, item):
+    begin_index = 0
+    end_index = len(sequence) - 1
+
+    while begin_index <= end_index:
+        midpoint = begin_index + (end_index - begin_index) // 2
+        midpoint_value = sequence[midpoint]
+        if midpoint_value == item:
+            return midpoint
+        elif item < midpoint_value:
+            end_index = midpoint - 1
+        else:
+            begin_index = midpoint + 1
+
+    return None
+
+
+print(bin_search([2,4,5,6,7,8,9,10,12,13,14], 12))
